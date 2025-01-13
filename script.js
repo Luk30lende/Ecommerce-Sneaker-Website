7ea0fb749074d3cee1d1d0a5e31b0e6a4cb780c4
Luke Olende <olendeluke2k@gmail.com>
2025-01-13 11:14:08 +0300

    Add background header changes

--- script.js
+++ script.js
@@ -53,3 +53,10 @@ swiperImages.controller.control = swiperTitles;
 swiperTitles.controller.control = swiperImages;
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
